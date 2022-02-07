033131d8b05e17b668020fa720c3ef8901997712
JovanCorilic <kssbc39@gmail.com>
2022-02-07 21:45:43 +0100

    kraj pisanja

--- SCADA/ReportManager/Program.cs
+++ SCADA/ReportManager/Program.cs
@@ -20,6 +20,7 @@ namespace ReportManager
                 "4.Poslednja vrednost svih AI tagova\n5.Poslednja vrednost svih DI tagova\n" +
                 "6.Sve vrednosti taga sa određenim identifikatorom");
                 int broj = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                Console.WriteLine();
                 if (broj == 0)
                     break;
                 else if (broj == 1)
@@ -32,23 +33,25 @@ namespace ReportManager
                     Console.WriteLine("Unesite kranji datum (primer: 20.12.2021 11:00:00)");
                     string krajni = Console.ReadLine();
                     DateTime temp2 = DateTime.Parse(krajni, culture);
-                    var lista = proxy.sviAlarmiOdredjenVremPeriod(temp1,temp2);
-                    foreach(var temp in lista)
+                    var lista = proxy.sviAlarmiOdredjenVremPeriod(temp1, temp2);
+                    foreach (var temp in lista)
                         Console.WriteLine(temp.ToString());
-                    while (true) {
+                    while (true)
+                    {
                         Console.WriteLine("Sortiranje podataka po:\n1.Prioritet\n2.Vreme\n0.Natrag");
                         int broj1 = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                        Console.WriteLine();
                         if (broj1 == 0)
                             break;
-                        else if(broj1 == 1)
+                        else if (broj1 == 1)
                         {
                             var listaSordt = lista.OrderBy(m => int.Parse(m.prioritet));
-                            foreach(var temp in listaSordt)
+                            foreach (var temp in listaSordt)
                             {
                                 Console.WriteLine(temp.ToString());
                             }
                         }
-                        else if(broj1 == 2)
+                        else if (broj1 == 2)
                         {
                             var listaSort = lista.OrderBy(m => m.vreme_aktivacije);
                             foreach (var temp in listaSort)
@@ -58,7 +61,140 @@ namespace ReportManager
                         }
                     }
                 }
-                
+                else if (broj == 2)
+                {
+                    Console.WriteLine("Unesite prioritet alarma:");
+                    string prioritet = Console.ReadLine();
+                    var lista = proxy.sviAlarmiOdredjenogPrioriteta(prioritet);
+                    foreach (var temp in lista)
+                    {
+                        Console.WriteLine(temp.ToString());
+                    }
+                    while (true)
+                    {
+                        Console.WriteLine("Sortiranje podataka po:\n1.Vreme\n0.Natrag");
+                        int broj1 = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                        Console.WriteLine();
+                        if (broj1 == 0)
+                            break;
+                        else if (broj1 == 1)
+                        {
+                            var listaSort = lista.OrderBy(m => m.vreme_aktivacije);
+                            foreach(var temp in listaSort)
+                            {
+                                Console.WriteLine(temp.ToString());
+                            }
+                        }
+                    }
+                }
+                else if (broj == 3)
+                {
+                    Console.WriteLine("Unesite pocetni datum (primer: 20.12.2021 11:00:00)");
+                    string pocetni = Console.ReadLine();
+                    CultureInfo culture = new CultureInfo("sr-RS");
+                    DateTime temp1 = DateTime.Parse(pocetni, culture);
+
+                    Console.WriteLine("Unesite kranji datum (primer: 20.12.2021 11:00:00)");
+                    string krajni = Console.ReadLine();
+                    DateTime temp2 = DateTime.Parse(krajni, culture);
+                    var lista = proxy.sveVrednostiTagovaUOdredjenomVremenu(temp1, temp2);
+                    foreach (var temp in lista)
+                        Console.WriteLine(temp.ToString());
+                    while (true)
+                    {
+                        Console.WriteLine("Sortiranje podataka po:\n1.Vreme\n0.Natrag");
+                        int broj1 = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                        Console.WriteLine();
+                        if (broj1 == 0)
+                            break;
+                        else if (broj1 == 1)
+                        {
+                            var listaSordt = lista.OrderBy(m => m.vreme_kreacije);
+                            foreach (var temp in listaSordt)
+                            {
+                                Console.WriteLine(temp.ToString());
+                            }
+                        }
+                        
+                    }
+                }
+                else if(broj == 4)
+                {
+                    var lista = proxy.poslednjaVrednostAItaga();
+                    foreach(var temp in lista)
+                    {
+                        Console.WriteLine(temp.ToString());
+                    }
+                    while (true)
+                    {
+                        Console.WriteLine("Sortiranje podataka po:\n1.Vreme\n0.Natrag");
+                        int broj1 = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                        Console.WriteLine();
+                        if (broj1 == 0)
+                            break;
+                        else if (broj1 == 1)
+                        {
+                            var listaSordt = lista.OrderBy(m => m.vreme_kreacije);
+                            foreach (var temp in listaSordt)
+                            {
+                                Console.WriteLine(temp.ToString());
+                            }
+                        }
+
+                    }
+                }
+                else if(broj == 5)
+                {
+                    var lista = proxy.poslednjaVrednostDItaga();
+                    foreach (var temp in lista)
+                    {
+                        Console.WriteLine(temp.ToString());
+                    }
+                    while (true)
+                    {
+                        Console.WriteLine("Sortiranje podataka po:\n1.Vreme\n0.Natrag");
+                        int broj1 = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                        Console.WriteLine();
+                        if (broj1 == 0)
+                            break;
+                        else if (broj1 == 1)
+                        {
+                            var listaSordt = lista.OrderBy(m => m.vreme_kreacije);
+                            foreach (var temp in listaSordt)
+                            {
+                                Console.WriteLine(temp.ToString());
+                            }
+                        }
+
+                    }
+                }
+                else if(broj == 6)
+                {
+                    Console.WriteLine("Unesite naziv identifikatora taga:");
+                    string naziv = Console.ReadLine();
+                    var lista = proxy.sveVrednostiSaOdredjenimIdentifikatorom(naziv);
+                    foreach (var temp in lista)
+                    {
+                        Console.WriteLine(temp.ToString());
+                    }
+                    while (true)
+                    {
+                        Console.WriteLine("Sortiranje podataka po:\n1.Vrednost\n0.Natrag");
+                        int broj1 = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
+                        Console.WriteLine();
+                        if (broj1 == 0)
+                            break;
+                        else if (broj1 == 1)
+                        {
+                            var listaSordt = lista.OrderBy(m => m.vrednost);
+                            foreach (var temp in listaSordt)
+                            {
+                                Console.WriteLine(temp.ToString());
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
